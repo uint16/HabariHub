9b1375afc89eeb6286d93853b0ece9685683c9cb
SiahL <slyimo@css.edu>
2014-08-03 14:36:39 -0700

    Removed unused imports

--- app/src/main/java/com/tanzoft/habarihub/rss/FeedSearch.java
+++ app/src/main/java/com/tanzoft/habarihub/rss/FeedSearch.java
@@ -1,14 +1,10 @@
 package com.tanzoft.habarihub.rss;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.tanzoft.habarihub.R;
 import com.tanzoft.habarihub.Settings.SettingsActivity;
